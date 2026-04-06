2a1b7a854c25f277fb90f66faca1920b1b3a4c23
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-05 20:48:36 -0600

    Made one cell.
    Initial Dom manipulation successful.

---
+++ j.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const cell = document.createElement("div");
+cell.setAttribute(
+  "style",
+  "background-color: black; width: 50px; height: 50px;",
+);
+container.appendChild(cell);
