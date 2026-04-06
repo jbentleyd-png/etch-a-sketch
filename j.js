8a9b58c68530d2576067c68df912a38b1f86caaf
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-05 21:02:40 -0600

    Made did "grid", minus spacing.
    For loop upon itself.

--- j.js
+++ j.js
@@ -1,14 +1,15 @@
 const container = document.querySelector(".container");
 
 function generateGrid(length) {
-  for (let i = 0; i < length; i++) {
-    const cell = document.createElement("div");
-    cell.setAttribute(
-      "style",
-      "background-color: black; width: 50px; height: 50px; margin: 5px;",
-    );
-    container.appendChild(cell);
+  for (let h = 0; h < length; h++) {
+    for (let l = 0; l < length; l++) {
+      const cell = document.createElement("div");
+      cell.setAttribute(
+        "style",
+        "background-color: black; width: 50px; height: 50px; margin: 5px;",
+      );
+      container.appendChild(cell);
+    }
   }
 }
-
-generateGrid(16);
+generateGrid(12);
